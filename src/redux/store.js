8a380c252fb9e5b5e3056a1424e0eb5d37caef90
Quentin Ikeno <ikenoq@gmail.com>
2022-08-03 17:17:05 -1000

    Fix name of reducer.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "./todosSlice";
 
-export default store = configureStore({ reducer: { todos: todosReducer } });
+export const store = configureStore({
+	reducer: {
+		todos: todosReducer,
+	},
+});
