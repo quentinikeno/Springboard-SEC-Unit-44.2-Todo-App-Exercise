530d7625a6eb2eb8028cd80ec9d51c680dd833a3
Quentin Ikeno <ikenoq@gmail.com>
2022-08-03 17:20:00 -1000

    Move the hero into it's own componenent.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./Hero";
 import TodosList from "./TodosList";
 import AddTodoForm from "./AddTodoForm";
 import "./App.css";
@@ -5,12 +6,7 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<section className="hero is-primary">
-				<div className="hero-body">
-					<h1 className="title">Todo List</h1>
-					<p className="subtitle">Now with Redux!</p>
-				</div>
-			</section>
+			<Hero />
 			<section className="container">
 				<AddTodoForm />
 				<TodosList />
---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+const Hero = () => {
+	return (
+		<section className="hero is-primary">
+			<div className="hero-body">
+				<h1 className="title">Todo List</h1>
+				<p className="subtitle">Now with Redux!</p>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
