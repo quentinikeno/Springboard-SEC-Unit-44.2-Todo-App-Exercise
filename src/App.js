c4a6c206d21b627fd022bbae6dc642b4fa133d83
Quentin Ikeno <ikenoq@gmail.com>
2022-08-03 17:18:01 -1000

    Render the TodosList and AddTodoForm.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import TodosList from "./TodosList";
+import AddTodoForm from "./AddTodoForm";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<section className="hero is-primary">
+				<div className="hero-body">
+					<h1 className="title">Todo List</h1>
+					<p className="subtitle">Now with Redux!</p>
+				</div>
+			</section>
+			<section className="container">
+				<AddTodoForm />
+				<TodosList />
+			</section>
+		</div>
+	);
 }
 
 export default App;
